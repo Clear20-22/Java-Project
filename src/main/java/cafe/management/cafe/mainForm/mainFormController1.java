251af88e24d38f4157335d184fde87be9f15f001
Suraya Jannat Mim <150699804+Mim1726@users.noreply.github.com>
2025-01-25 19:01:53 +0600

    Update mainFormController1.java

--- src/main/java/cafe/management/cafe/mainForm/mainFormController1.java
+++ src/main/java/cafe/management/cafe/mainForm/mainFormController1.java
@@ -1,10 +1,14 @@
 package cafeshopmanagementsystem;
 
+import com.sun.jdi.connect.spi.Connection;
+import java.beans.Statement;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 import java.util.ResourceBundle;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
@@ -19,6 +23,7 @@ import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
@@ -83,6 +88,9 @@ public class mainFormController1 implements Initializable{
 
     @FXML
     private ComboBox<?> inventory_status;
+    
+    @FXML
+    private ComboBox<?> inventory_type;
 
     @FXML
     private TextField inventory_stock;
@@ -105,7 +113,29 @@ public class mainFormController1 implements Initializable{
     @FXML
     private Label username;
     
+    //
+    
+    
+  
+    
+    
     private Alert alert;
+    
+    private Connection connect;
+    private PreparedStatement prepare;
+    private Statement statement;
+    private ResultSet result;
+    
+    
+    
+    /*
+    public void inventoryAddBtn(){
+        
+        if(inventory_product.ID)
+    }
+    */
+    
+    
     
     private String[] typeList={"Meals", "Drinks"};
     
@@ -115,6 +145,23 @@ public class mainFormController1 implements Initializable{
         for(String data: typeList){
             typeL.add(data);
         }
+        
+        ObservableList listData=FXCollections.observableArrayList(typeL);
+        inventory_type.setItems(listData);
+    }
+    
+    private String[] statusList={"Available","Unavailable"};
+    public void inventoryStatusList(){
+        
+        List<String> statusL=new ArrayList<>();
+        
+        for(String data: statusList){
+            statusL.add(data);
+        }
+        
+        ObservableList listData= FXCollections.observableArrayList(statusL);
+        inventory_status.setItems(listData);
+        
     }
     
     public void logout(){
@@ -149,15 +196,20 @@ public class mainFormController1 implements Initializable{
         }
     
     }
+    
     public void displayUsername(){
         String user=data.username;
         user=user.substring(0,1).toUpperCase()+user.substring(1);
         username.setText(user);
     }
+    
     @Override
     public void initialize(URL url, ResourceBundle resources){
         
         displayUsername();
-    }
         
+        inventoryTypeList();
+        
+        inventoryStatusList();
+    }     
 }
