9c2c713d6c6006a38a9879c986b06ede21925477
Clear20-22 <151145553+Clear20-22@users.noreply.github.com>
2025-01-27 22:05:45 +0600

    Add files via upload

---
+++ src/main/java/cafe/management/cafe/CafeManagement.java
@@ -0,0 +1,37 @@
+package cafe.management.cafe;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class CafeManagement extends Application {
+
+    private static Stage Samestage;
+
+    @Override
+    public  void start(Stage stage) throws IOException {
+        Samestage = stage;
+        FXMLLoader fxmlLoader = new FXMLLoader(CafeManagement.class.getResource("LoginPage.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 800, 600);
+        stage.setTitle("Cafe Management");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void switchScene(String fxmlFile,int width) throws IOException {
+        FXMLLoader loader = new FXMLLoader(CafeManagement.class.getResource(fxmlFile));
+        Scene scene = new Scene(loader.load(), width, 600);
+        Samestage.setScene(scene);
+        Samestage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
